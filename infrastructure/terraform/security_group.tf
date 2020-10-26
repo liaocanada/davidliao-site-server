9c814852e2fffbc63e3c3a3413936b7a990034a6
David Liao <liao.canada@gmail.com>
2020-10-26 18:41:18 -0400

    Separate terraform resources into different files

--- infrastructure/terraform/security_group.tf
+++ infrastructure/terraform/security_group.tf
@@ -1,35 +1,3 @@
-provider "aws" {
-  profile = "default"
-  region  = "us-east-1"
-}
-
-resource "aws_instance" "webserver" {
-  ami               = "ami-0758470213bdd23b1" # Ubuntu 20.04
-  instance_type     = "t2.micro"
-  availability_zone = "us-east-1a"
-  key_name          = "aws-us-east"
-  security_groups   = ["${aws_security_group.main.name}"]
-
-  root_block_device {
-    volume_type = "gp2"
-    volume_size = 8
-  }
-
-  volume_tags = {
-    Name = "DavidliaoWebServer"
-  }
-
-  tags = {
-    Name = "DavidliaoWebServer"
-    Apps = "DavidliaoSite"
-  }
-}
-
-resource "aws_eip" "ip" {
-  vpc      = true
-  instance = aws_instance.webserver.id
-}
-
 resource "aws_security_group" "main" {
   name        = "DevWebServerSG"
   description = "Allows SSH, various custom TCP ports, and ICMP for ping."
